1f8b75feda388da7720125c381021b7933bcf556
Lev Grossman <levgrossman@Levs-MacBook-Pro-2.local>
2017-02-26 12:32:05 -0500

    a

--- bot.py
+++ bot.py
@@ -5,6 +5,13 @@ import sys
 import socket
 import json
 
+
+buys = [{"type": "add", "order_id": 0, "symbol": "BOND", "dir": "BUY", "price": 1000, "size": 2},
+        {"type": "add", "order_id": 0, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 5},
+        {"type": "add", "order_id": 0, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 2},
+        {"type": "add", "order_id": 0, "symbol": "BOND", "dir": "BUY", "price": 997, "size": 1}]
+
+
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("test-exch-codebrew", 25000))
@@ -25,9 +32,10 @@ def main():
 
     n_bonds = 0
     order_id = 0
-    write(exchange, {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", 
-                     "price": 1000, 
-                     "size": 10})
+
+    write(exchange, buy(0, order_id, 2))
+    order_id += 1
+
     while True:
         try:
             message = read(exchange)
@@ -37,15 +45,17 @@ def main():
 
         if 'type' in message and message['type'] == "fill": #filling order
           n_bonds += message["size"]
-          print(message, file=sys.stderr)
-        elif 'symbol' in message and message['symbol'] == "BOND" and 'sell' in message:
-            if to_buy(message['sell'])[0] > 0:
-                print(message, file=sys.stderr)
+          print("Bought ", message["size"], " BONDs @ ", message["price"])
+          write(exchange, buy(0, order_id, message["size"]))
+          #print(message, file=sys.stderr)
+        #elif 'symbol' in message and message['symbol'] == "BOND" and 'sell' in message:
+        #    if to_buy(message['sell'])[0] > 0:
+                #print(message, file=sys.stderr)
                 #write(exchange, {"type": "add", "order_id": order_id, "symbol": "BOND", "dir": "BUY", 
                 #                 "price": to_buy(message['sell'])[0], 
                 #                 "size": to_buy(message['sell'])[1]})
                 #order_id += 1
-                print(n_bonds)
+                #print(n_bonds)
         elif 'type' in message:
             if message['type'] == "ack" or message['type'] == "reject":
                 print(message, file=sys.stderr)
@@ -59,6 +69,10 @@ def to_buy(message):
     return [0,0]
             #Buy message['sell'][i][1] of them
 
+def buy(index, order_id, n):
+  tmp = buys[index]
+  tmp["order_id"] = order_id; tmp["size"] = n
+  return tmp
 
 if __name__ == "__main__":
     main()
